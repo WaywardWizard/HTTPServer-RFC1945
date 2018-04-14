3025fa2a49baeae5d39447e126118f9c2d3a76ca
Ben Tomlin <btomlin@student.unimelb.edu.au>
2018-04-14 18:44:09 +1000

    Add get method, and method to send response structure

--- http/http.c
+++ http/http.c
@@ -1,106 +1,175 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
 #include "httpStructures.h"
 #include "../utility/tcpSocketIo.h"
-#include <stdlib.h>
+#include "../utility/filesystem.h"
+#include "../utility/logger.h"
+#include "../utility/regexTool.h"
+#include "http.h"
 
-request_t
-*readRequest(int socketFd) {
+#define MIME_JS "javascript"
+#define MIME_HTML "html"
+#define MIME_CSS "css"
+#define MIME_JPEG "jpeg"
+#define MIME_DEFAULT "application/octet-stream$"
+#define MATCH_MIME_JS "\\.js$"
+#define MATCH_MIME_HTML "\\.html$"
+#define MATCH_MIME_CSS "\\.css$"
+#define MATCH_MIME_JPEG "\\.jpg$"
+
+#define EINVALID_REQUEST 19 // Request was malformed
+
+void _handleInvalidPath();
+
+int _parseRequestLine(char* requestLine, request_t *r);
+response_t* _getResponse(request_t *r, int socketFd, char* rootPath);
+request_t *_getRequest(int socketFd);
+response_t *_httpGet(request_t *r, char* rootPath);
+char* _getMimeType(char* fPath);
+char* _compilePathFromURI(char* uri, char* rootPath);
+
+void _notifyInvalidRequest();
+void _parseRequestHeader(char* headerLine, request_t *r);
+void _parseRequestEntity(request_t* r, int socketFd);
+void _sendResponse(response_t* r, int socketFd);
+
+
+void processRequest(int socketFd, char* rootPath) {
+
+	/* Read request, assemble response */
+	request_t* r=_getRequest(socketFd);
+	response_t* rs=_getResponse(r, socketFd, rootPath);
+	freeRequest(r);free(r);
+
+	/* Send the response and close up */
+	_sendResponse(rs, socketFd);
+	freeResponse(rs);free(rs);
+}
+
+request_t *_getRequest(int socketFd) {
 	/**
 	 * Read a request from a connected socket & return a request structure
 	 */
+
 	request_t* r=initRequest();
 	char* requestLine = fdReadLine(socketFd);
 
 	if(requestLine==NULL){
-		notifyInvalidRequest();
+		handleInvalidRequest();
 	}
 
 	/* Read request line */
-	parseRequestLine(requestLine, r);
+	_parseRequestLine(requestLine, r);
 	free(requestLine);
 
 	/* Read request header fields */
-	while(strcmp((requestLine=fdReadLine(socketFd)), '')!=0) {
+	while((requestLine=fdReadLine(socketFd))!=NULL) {
 
 		/* Not implemented */
-		parseRequestHeader(requestLine, r);
+		_parseRequestHeader(requestLine, r);
 	}
 
-	/* Not implemented */
-	parseRequestEntity();
+	/* Not implemented. Reads content-length bytes from fd */
+	_parseRequestEntity(r, socketFd);
 
 	return(r);
 }
 
 
-void
-replyRequest(request_t *r, int socketFd, char* rootPath) {
+response_t*
+_getResponse(request_t *r, int socketFd, char* rootPath) {
 	/**
 	 * Reply to a parsed request, via socketFd
 	 *
 	 * ARGUMENT:
+	 * 		request_t r - The request. Should be parsed/populated
 	 * 		socketFd - file descriptor for open socket
 	 *
 	 * NOTE:
-	 * 		Only the GET method is supported for  the assignment
+	 * 		Only the GET method is supported (for the assignment)
 	 */
 	char *resourcePath=NULL;
-	switch(r->method) {
-	case "GET":
-		httpGet(r, rootPath);
-		break;
+	response_t *rs;
+	if(strcmp(r->method,"GET")==0) {
+		rs = _httpGet(r, rootPath);
 	}
+	return(rs);
 }
 
 
-void
-httpGet(request_t r, char* rootPath) {
+response_t*
+_httpGet(request_t *r, char* rootPath) {
 
-	response_t response;
-	char* status;
-	switch(r->httpVersion) {
-		case "HTTP/0.9":
-			httpGet0p9(r, rootPath);
-			break;
-		case "HTTP/1.0":
-			httpGet1p1(r, rootPath);
-			break;
-	}
-	char* resourcePath=compilePathFromURI(r->uri, rootPath);
-
-	/* Check the file is readable */
-	if((testPath(resourcePath, R_OK))==FALSE) {
-		/* Print error message and close up */
-		handleInvalidRequest();
-	}
+	response_t* response;
+	char* statusCode;
+	char* statusPhrase;
+	char* resourcePath=_compilePathFromURI((r->uri), rootPath);
+	char* contentType;
 
 	/* Check the file exists */
-	if(testPath(resourcePath, F_OK)==FALSE) {
-		status = "404"; 		//
+	if(testFile(resourcePath, F_OK|R_OK)==FALSE) {
+		statusCode = "404";
+		statusPhrase="Not Found";
+
 	} else {
-		status = "200";
+		statusCode = "200";
+		statusPhrase="OK";
 	}
+
+	/* Set status and phrase */
+	response->status->code=malloc(strlen(statusCode)+1);
+	response->status->phrase=malloc(strlen(statusPhrase)+1);
+	strcpy(response->status->code, statusCode);
+	strcpy(response->status->phrase, statusPhrase);
+
+	/* TODO Check if this needs to bee free'd*/
+	contentType=_getMimeType(resourcePath);
+	response->eHeader->contentType=contentType;
+
+	response->entityPath=resourcePath;
+
+	return(response);
+}
+
+
+char* _getMimeType(char* fPath) {
+	/**
+	 * Given a filepath, return it's mime type based on its file extension
+	 */
+	if (isMatch(""MATCH_MIME_JS, fPath)) {
+		return(MIME_JS);
+	} else if (isMatch(""MATCH_MIME_HTML, fPath)) {
+		return(MIME_HTML);
+	} else if (isMatch(""MATCH_MIME_JPEG, fPath)) {
+		return(MIME_JPEG);
+	} else if (isMatch(""MATCH_MIME_CSS, fPath)) {
+		return(MIME_CSS);
+	}
+	return(""MIME_DEFAULT);
 }
 
 
 void
-parseRequestHeader(char* headerLine, request_t *r) {}
+_parseRequestHeader(char* headerLine, request_t *r) {return;}
 
 
 char*
-compilePathFromURI(char* uri, char* rootPath) {
+_compilePathFromURI(char* uri, char* rootPath) {
 	/**
 	 * Given a URI and server root path, resolve to an absolute path
 	 * Remove query and params part of the URI if any
 	 *
 	 * NOTE:
-	 * 		Caller is responsible for freeing the compiled path
+	 * 		Caller is responsible for freeing the compiled path that is returned
 	 */
 	int rPathLength=strlen(rootPath);
 	char* uriPath;
 
 	/* No match found */
 	if(extractMatch(PATH, uri, &uriPath)==NULL) {
-		handleInvalidPath();
+		_handleInvalidPath();
 	}
 
 	char* path = malloc(strlen(rootPath)+strlen(uri));
@@ -111,14 +180,20 @@ compilePathFromURI(char* uri, char* rootPath) {
 
 
 void
-notifyInvalidRequest() {
+_handleInvalidPath() {
+	mylog("Could not extract a path from the given uri");
+	exit(1);
+}
+
+void
+_notifyInvalidRequest() {
 	mylog("Request invalid.");
 	exit(1);
 }
 
 
 int
-parseRequestLine(char* requestLine, request_t *r) {
+_parseRequestLine(char* requestLine, request_t *r) {
 	/**
 	 * Load http1.0 request line into request structure. If the request is
 	 * malformed, return EINVALID_REQUEST and write null bytes into request struct
@@ -143,3 +218,29 @@ parseRequestLine(char* requestLine, request_t *r) {
 	return(0);
 }
 
+
+void _parseRequestEntity(request_t* r, int socketFd){}
+
+
+void _sendResponse(response_t* r, int socketFd) {
+	/* Serialize response structure and pipe it into socketFd */
+
+	/* Status line */
+	sendString(socketFd, r->httpVersion, ' ');
+	sendString(socketFd, r->status->code, ' ');
+	sendString(socketFd, r->status->phrase, '\n');
+	sendChar(socketFd, '\n');
+
+	/* Response header */
+	sendString(socketFd, "Content-Type", ':');
+	sendString(socketFd, r->eHeader->contentType, '\n');
+
+	/* Send Entity */
+	FILE *f = fopen(r->entityPath, "r");
+	if(f==NULL){
+		handleFileOpenError();
+	}
+	sendFile(socketFd, f);
+	fclose(f);
+
+}
--- http/http.h
+++ http/http.h
@@ -1,8 +1,44 @@
 /*
  * http.h
  *
- *  Created on: 11 Apr. 2018
+ *  Created on: 13 Apr. 2018
  *      Author: ben
  */
 
+#ifndef HTTP_HTTP_H_
+#define HTTP_HTTP_H_
 
+#define METHOD_REGEX "^GET"
+#define URI_REGEX    "^([[:space:]]){1}"RELURI
+
+/* \\, escape once for c to get a literal \ */
+#define HTTP_VERSION_REGEX "HTTP/[[:digit:]]+\\.[[:digit:]]+"
+
+/* Regex building blocks as per RFC1945 */
+#define CTL			 "[:cntrl:]"
+#define ALPHANUM      "[:alpha:][:digit:]"
+#define SP			 "[:space:]"
+#define HEX			 "A-Fa-f[:digit:]"
+#define SAFE			 "$_.-"
+#define UNSAFE		 CTL SP "#%<>\""
+#define EXTRA		 "!*'(),"
+#define NATIONAL      "[^"RESERVED EXTRA UNSAFE ALPHANUM SAFE "]"
+#define ESCAPE		 "(%["HEX"]{2})"
+#define RESERVED		 ";/@?:&=+"
+#define UNRESERVED	 "["ALPHANUM EXTRA SAFE"]|"NATIONAL
+#define UCHAR 		 "("UNRESERVED"|"ESCAPE")"
+#define PCHAR		 "([:@+=&]|"UCHAR")"
+#define SEGMENT 		 PCHAR "*"
+#define FSEGMENT		 PCHAR "+"
+#define PATH			 "("FSEGMENT "(/" SEGMENT ")*)"
+#define PARAM		 "("PCHAR"|/)*"
+#define PARAMS		 PARAM"(;"PARAM")*"
+#define QUERY		 "(" UCHAR "|[" RESERVED "])*"
+#define RELPATH		 "("PATH"?(;"PARAMS")?(([\?])"QUERY")?)"
+#define ABSPATH		 "(/"RELPATH")"
+#define RELURI	     "("ABSPATH"|"RELPATH")"
+
+void processRequest(int socketFd, char* rootPath);
+
+
+#endif /* HTTP_HTTP_H_ */

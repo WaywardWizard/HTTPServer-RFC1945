1753e90710f07cd801ba3c252c5d8e361482d4c7
Ben Tomlin <btomlin@student.unimelb.edu.au>
2018-04-15 16:45:35 +1000

    Debug

--- http/http.c
+++ http/http.c
@@ -9,10 +9,10 @@
 #include "./../utility/regexTool.h"
 #include "http.h"
 
-#define MIME_JS "javascript"
-#define MIME_HTML "html"
-#define MIME_CSS "css"
-#define MIME_JPEG "jpeg"
+#define MIME_JS "application/javascript"
+#define MIME_HTML "text/html"
+#define MIME_CSS "text/css"
+#define MIME_JPEG "image/jpeg"
 #define MIME_DEFAULT "application/octet-stream$"
 #define MATCH_MIME_JS "\\.js$"
 #define MATCH_MIME_HTML "\\.html$"
@@ -37,48 +37,57 @@ void _sendResponse(response_t* r, int socketFd);
 
 
 void processRequest(int socketFd, char* rootPath) {
+	/**
+	 * Process an http request on socketFd. Send response back through socket.
+	 *
+	 * Respond only to valid GET requests, only with 404 or 200 status, only
+	 * for mime types as specified in the assignment
+	 */
 
 	/* Read request, assemble response */
 	request_t* r=_getRequest(socketFd);
 	if(r==NULL) {
 		mylog("Malformed request");
+
+		/* No handling required for malformed requests in assignment */
+		return;
 	}
+
 	response_t* rs=_getResponse(r, socketFd, rootPath);
 	freeRequest(r);free(r);
 
-	/* Send the response and close up */
+	/* Send the response.*/
 	_sendResponse(rs, socketFd);
 	freeResponse(rs);free(rs);
 }
+
 
 request_t *_getRequest(int socketFd) {
 	/**
 	 * Read a request from <socketFd> & return a request structure
 	 *
-	 * Return null if no request could be read
+	 * Return null if no valid request could be read
 	 */
 
 	request_t* r=initRequest();
+
+	/* Read request line from socket */
 	char* requestLine = fdReadLine(socketFd);
-
 	if(requestLine==NULL){
-		return(NULL);
-	}
-
-	/* Read request line */
-	if(_parseRequestLine(requestLine, r)==EINVALID_REQUEST) {
+		freeRequest(r);free(r);
 		free(requestLine);
 		return(NULL);
 	}
 
-	/**TODO: Stop from looping, needs to quit on finding an empty line */
-	/* Read request header fields
-	while((requestLine=fdReadLine(socketFd))!=NULL) {
-
-		/* Not implemented
-		_parseRequestHeader(requestLine, r);
+	/* Parse request line to request structure*/
+	if(_parseRequestLine(requestLine, r)==EINVALID_REQUEST) {
+		freeRequest(r);free(r);
+		free(requestLine);
+		return(NULL);
 	}
-	*/
+
+	/* Not implemented*/
+	_parseRequestHeader(requestLine, r);
 
 	/* Not implemented. Reads content-length bytes from fd */
 	_parseRequestEntity(r, socketFd);
@@ -93,13 +102,19 @@ _getResponse(request_t *r, int socketFd, char* rootPath) {
 	 * Reply to a parsed request, via socketFd
 	 *
 	 * ARGUMENT:
-	 * 		request_t r - The request. Should be parsed/populated
+	 * 		request_t r - The request. Should be parsed/populated with ateast
+	 * 		a request line
+	 *
 	 * 		socketFd - file descriptor for open socket
+	 *
+	 * RETURN:
+	 * 		response_t *r - response structure representing server response.
+	 * 		The caller is responsible for freeing this.
 	 *
 	 * NOTE:
 	 * 		Only the GET method is supported (for the assignment)
 	 */
-	char *resourcePath=NULL;
+
 	response_t *rs=initResponse();
 	rs->httpVersion=strdup(r->httpVersion);
 
@@ -107,11 +122,24 @@ _getResponse(request_t *r, int socketFd, char* rootPath) {
 	if(strcmp(r->method,"GET")==0) {
 		_httpGet(r, rs, rootPath);
 	}
+
 	return(rs);
 }
 
 
 void _httpGet(request_t *request, response_t *response, char* rootPath) {
+	/**
+	 * Handle a get request. Populate response structure for that request
+	 *
+	 * ARGUMENT:
+	 * 	request - request structure representing reqeust
+	 * 	response - response structure to populate, representing the server
+	 * 	response.
+	 * 	rootPath - path to server document root
+	 *
+	 * RETURN:
+	 * 	Mutate response structure
+	 */
 
 	request_t*r=request;
 	char* statusCode;
@@ -127,6 +155,8 @@ void _httpGet(request_t *request, response_t *response, char* rootPath) {
 	} else {
 		statusCode = "200";
 		statusPhrase="OK";
+		response->eHeader->contentType=strdup(_getMimeType(resourcePath));
+		response->entityPath=resourcePath;
 	}
 
 	/* Set status and phrase */
@@ -134,13 +164,6 @@ void _httpGet(request_t *request, response_t *response, char* rootPath) {
 	response->status->phrase=malloc(strlen(statusPhrase)+1);
 	strcpy(response->status->code, statusCode);
 	strcpy(response->status->phrase, statusPhrase);
-
-	/* TODO Check if this needs to bee free'd*/
-	contentType=_getMimeType(resourcePath);
-	response->eHeader->contentType=contentType;
-
-	response->entityPath=resourcePath;
-
 }
 
 
@@ -172,10 +195,11 @@ _compilePathFromURI(char* uri, char* rootPath) {
 	 * Remove query and params part of the URI if any
 	 *
 	 * NOTE:
-	 * 		Caller is responsible for freeing the compiled path that is returned
+	 * 	Caller is responsible for freeing the compiled path that is returned
 	 *
 	 * RETURN:
 	 * 	allocated filepath of the uri. Null if cannot extract a valid filepath.
+	 *
 	 */
 	int rPathLength=strlen(rootPath);
 	char* uriPath;
@@ -202,6 +226,7 @@ _handleInvalidPath() {
 	exit(1);
 }
 
+
 void
 _notifyInvalidRequest() {
 	mylog("Request invalid.");
@@ -212,7 +237,7 @@ _notifyInvalidRequest() {
 int
 _parseRequestLine(char* requestLine, request_t *r) {
 	/**
-	 * Load http1.0 request line into request structure. If the request is
+	 * Load http request line into request structure. If the request is
 	 * malformed, return EINVALID_REQUEST and write null bytes into request struct
 	 */
 
@@ -245,22 +270,28 @@ void _parseRequestEntity(request_t* r, int socketFd){}
 void _sendResponse(response_t* r, int socketFd) {
 	/* Serialize response structure and pipe it into socketFd */
 
-	/* Status line */
-	sendString(socketFd, r->httpVersion, " ");
-	sendString(socketFd, r->status->code, " ");
-	sendString(socketFd, r->status->phrase, "\n");
+	/* Send a simple request (only the entity) if http0.9 */
+	if(strcmp(r->httpVersion, "HTTP/0.9")!=0) {
+		/* Status line */
+		sendString(socketFd, r->httpVersion, " ");
+		sendString(socketFd, r->status->code, " ");
+		sendString(socketFd, r->status->phrase, "\n");
 
-	/* Response header */
-	sendString(socketFd, "Content-Type", ":");
-	sendString(socketFd, r->eHeader->contentType, "\n");
-
-	/* Send Entity */
-	sendChar(socketFd, "\n");
-	FILE *f = fopen(r->entityPath, "r");
-	if(f==NULL){
-		handleFileOpenError();
+		/* Send headers for entity if entity exists */
+		if (strcmp(r->status->code, "404")!=0) {
+			sendString(socketFd, "Content-Type:", " ");
+			sendString(socketFd, r->eHeader->contentType, "\n");
+			sendChar(socketFd, "\n");
+		}
 	}
-	sendFile(socketFd, f);
-	fclose(f);
 
+	/* Send Entity if exists*/
+	if (r->entityPath!=NULL) {
+		FILE *f = fopen(r->entityPath, "rb");
+		if(f==NULL){
+			handleFileOpenError();
+		}
+		sendFile(socketFd, f);
+		fclose(f);
+	}
 }
